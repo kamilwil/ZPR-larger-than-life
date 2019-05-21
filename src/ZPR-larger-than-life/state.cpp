b5bdada3a14a72a65fbd29d91f41fcf987f23277
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-05-21 16:00:09 +0200

    Update state.cpp

--- src/ZPR-larger-than-life/state.cpp
+++ src/ZPR-larger-than-life/state.cpp
@@ -11,18 +11,36 @@
 
     State::State()=default;
     State::~State()=default;
-    State::State(std::set<Cell> actives, std::set<Cell> inactives, int it){}
+    State::State(std::set<Cell*> actives, std::set<Cell*> inactives, int it){
+        active_cells = actives;
+        inactive_cells = inactives;
+        it_number = it;
+    }
 
-    void State::changeIteration(int i){}
-    void State::addActiveCell(Cell cell){}
-    void State::removeActiveCell(Cell cell){}
-    void State::addInactiveCell(Cell cell){}
-    void State::removeInactiveCell(Cell cell){}
+//    void State::changeIteration(int i){}
+    void State::addActiveCell(Cell *cell){active_cells.insert(cell);}
 
-    std::set<Cell> State::get_activeCells(){return active_cells;}
-    std::set<Cell> State::get_inactiveCells(){return inactive_cells;}
-    int State::getItNumber(){return 1;}
-    void State::setActiveCells(std::set<Cell> actives){}
+    void State::removeActiveCell(Cell *cell){
+        for(auto it = active_cells.begin(); it != active_cells.end(); )
+            if(*it == cell)
+                it = active_cells.erase(it);
+            else
+                ++it;
+    }
+    void State::addInactiveCell(Cell *cell){inactive_cells.insert(cell);}
+
+    void State::removeInactiveCell(Cell *cell){
+        for(auto it = inactive_cells.begin(); it != inactive_cells.end(); )
+            if(*it == cell)
+                it = inactive_cells.erase(it);
+            else
+                ++it;        
+    }
+
+    std::set<Cell*> State::get_activeCells(){return active_cells;}
+    std::set<Cell*> State::get_inactiveCells(){return inactive_cells;}
+    int State::getItNumber(){return it_number;}
+    void State::setActiveCells(std::set<Cell> actives){active_cells = actives}
     void State::setInactiveCells(std::set<Cell> inactives){}
     void State::setItNumber(int it){}
 
