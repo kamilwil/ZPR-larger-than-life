d30889431ff67e7eda672e476bf357ad329bf8eb
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-05-21 16:00:14 +0200

    Update state.hpp

--- src/ZPR-larger-than-life/state.hpp
+++ src/ZPR-larger-than-life/state.hpp
@@ -9,25 +9,25 @@
 
 
 class State{
-    std::set<Cell> active_cells;
-    std::set<Cell> inactive_cells;
+    std::set<Cell*> active_cells;
+    std::set<Cell*> inactive_cells;
     int it_number;
 public:
     State();
     ~State();
-    State(std::set<Cell>, std::set<Cell>, int);
+    State(std::set<Cell*>, std::set<Cell*>, int);
 
-    void changeIteration(int);
-    void addActiveCell(Cell);
-    void removeActiveCell(Cell);
-    void addInactiveCell(Cell);
-    void removeInactiveCell(Cell);
+//    void changeIteration(int);
+    void addActiveCell(Cell*);
+    void removeActiveCell(Cell*);
+    void addInactiveCell(Cell*);
+    void removeInactiveCell(Cell*);
 
-    std::set<Cell> get_activeCells();
-    std::set<Cell> get_inactiveCells();
+    std::set<Cell> * get_activeCells();
+    std::set<Cell> * get_inactiveCells();
     int getItNumber();
-    void setActiveCells(std::set<Cell>);
-    void setInactiveCells(std::set<Cell>);
+    void setActiveCells(std::set<Cell*>);
+    void setInactiveCells(std::set<Cell*>);
     void setItNumber(int it);
 };
 
