22232486d161d8e7efb218e6b11b87dfe8b9ec6d
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-05-24 19:59:46 +0200

    Update game.cpp

--- src/ZPR-larger-than-life/game.cpp
+++ src/ZPR-larger-than-life/game.cpp
@@ -14,18 +14,19 @@
     Game::Game()=default;
     Game::~Game()=default;
 
-    Game::Game(std::map<int,int> rulings,std::set<Change*> change_list, State status){
+
+    Game::Game(Rules rulings,std::set<Change*> change_list, State status){
         rules = rulings;
         changes = change_list;
         state = status;
     }
 
 
-    std::map<int,int> Game::getRules(){return rules;}
+    Rules Game::getRules(){return rules;}
     std::deque<Change> Game::getChanges(){return changes;}
     State Game::getState(){return state;}
 
-    void Game::setRules(std::map<int,int> rulings){rules = rulings;}
+    void Game::setRules(Rules rulings){rules = rulings;}
     void Game::setChanges(std::set<Change> change_list){changes = change_list; }
     void Game::setState(State status){state = status;}
 
