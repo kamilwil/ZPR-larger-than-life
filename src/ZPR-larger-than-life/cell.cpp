663454442b1a8662410d8eade0989c7934238912
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-05-30 00:42:45 +0200

    Made coordinates std::pair

--- src/ZPR-larger-than-life/cell.cpp
+++ src/ZPR-larger-than-life/cell.cpp
@@ -1,16 +1,18 @@
 //Cell.cpp
 #include "cell.hpp"
+#include <pair>
 
     Cell::Cell() = default;
     Cell::~Cell() = default;
     Cell::Cell(int x, int y, int st){
-        x_coord = x;
-        y_coord = y;
+        coords = std::make_pair(x,y);
         state = st;
     }
         
-    int Cell::getXcoord(void){return x_coord;}
-    int Cell::getYcoord(void){return y_coord;}
+    void Cell::setCoords(x,y){coords.first = x; coords.second = y;}
+    std::pair<int,int> Cell::getCoords(){return coords;}
+    int Cell::getXcoord(void){return coords.first;}
+    int Cell::getYcoord(void){return coords.second;}
     void Cell::setXcoord(int x){x_coord = x;}
     void Cell::setYcoord(int y){y_coord = y;}
     void Cell::setState(int st){state = st;}
