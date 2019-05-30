bd0da1660918ba18f3d294c1b2f6fbb8dce6554b
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-05-30 11:51:04 +0200

    Funkcja updateRecord - redukuje ilosc kodu

--- src/ZPR-larger-than-life/game.cpp
+++ src/ZPR-larger-than-life/game.cpp
@@ -9,7 +9,7 @@
 #include <map>
 #include <pair>
 #include <iterator>
-#include <algorithm>  // min,max (da sie bez tego ale jest czytelniej niz konstrukcja a ? b : c )
+#include <algorithm>  // min,max (da sie bez tego ale jest czytelniej niz konstrukcja a ? b : c ; zgaduje za duzo pythona)
 #include <cmath>     //sqrt, floor, ceil
 
 
@@ -22,6 +22,18 @@ Game::Game(Rules rulings,std::set<Change*> change_list, State status){
     rules = rulings;
     changes = change_list;
     state = status;
+}
+
+void Game::updateRecord(std::map<std::pair<int, int>, int>* influence_map, int x_index, int y_index){
+    to_find = std::make_pair(x_index, y_index);
+    finder = (*influence_map).find(to_find)
+        
+    if finder == (*influence_map).end(){           // nie ma takiego klucza https://stackoverflow.com/questions/1939953/how-to-find-if-a-given-key-exists-in-a-c-stdmap
+        (*influence_map).insert(std::pair<to_find, 1);
+    }
+    else{                                       // jest taki klucz
+        (*influence_map)[to_find] += 1;
+    }    
 }
 
 void Game::includeCellInfluence(std::map<std::pair<int, int>, int>* influence_map, Cell* current_cell){
@@ -41,63 +53,35 @@ void Game::includeCellInfluence(std::map<std::pair<int, int>, int>* influence_ma
     max_x = max(BOARD_SIZE-1, current_x);        
     min_y = min(0, current_y);
     max_y = max(BOARD_SIZE-1, current_y);   
-    
-    
-        
-    if (rules.neighboorhood == NeighbourhoodType::MOORE){
+            
+    if (rules.neighboorhood == NeighbourhoodType::MOORE)
         for (auto x_offset = max(-rules.range, -current_x) ; x_offset <= min(rules.range, BOARD_SIZE-1-current_x); ++x_offset)
-             for (auto y_offset = max(-rules.range, -current_y) ; y_offset <= min(rules.range, BOARD_SIZE-1-current_y); ++y_offset) {
-                 to_find = std::make_pair(current_x + x_offset , current_y + y_offset);
-                 finder = (*influence_map).find(to_find)
-                 if finder == (*influence_map).end(){           // nie ma takiego klucza https://stackoverflow.com/questions/1939953/how-to-find-if-a-given-key-exists-in-a-c-stdmap
-                     (*influence_map).insert(std::pair<to_find, 1);
-                 }
-                 else{                                       // jest taki klucz
-                     (*influence_map)[to_find] += 1;
-                 }
-             }
-    }
-    else if (rules.neighboorhood == NeighbourhoodType::NEUM){
+             for (auto y_offset = max(-rules.range, -current_y) ; y_offset <= min(rules.range, BOARD_SIZE-1-current_y); ++y_offset) 
+                updateRecord(influence_map, current_x + x_offset, current_y + y_offset)
+    
+    else if (rules.neighboorhood == NeighbourhoodType::NEUM)
         for (auto x_offset = max(-rules.range, -current_x) ; x_offset <= min(rules.range, BOARD_SIZE-1-current_x); ++x_offset)
-             for (auto y_offset = -abs(rules.range - abs(x_offset)); y_offset <= abs(rules.range - abs(x_offset)); ++y_offset) {
-                 to_find = std::make_pair(i,j);
-                 finder = (*influence_map).find(to_find)
-                 if finder == (*influence_map).end(){           // nie ma takiego klucza https://stackoverflow.com/questions/1939953/how-to-find-if-a-given-key-exists-in-a-c-stdmap
-                     (*influence_map).insert(std::pair<to_find, 1);
-                 }
-                 else{                                       // jest taki klucz
-                     (*influence_map)[to_find] += 1;
-                 }
-             }        
+             for (auto y_offset = -abs(rules.range - abs(x_offset)); y_offset <= abs(rules.range - abs(x_offset)); ++y_offset) 
+                 updateRecord(influence_map, current_x + x_offset, current_y + y_offset)      
         
-    }
-    else if (rules.neighboorhood == NeighbourhoodType::CIRC){
+    else if (rules.neighboorhood == NeighbourhoodType::CIRC)
         for (auto x_offset = max(-rules.range, -current_x) ; x_offset <= min(rules.range, BOARD_SIZE-1-current_x); ++x_offset)
              for (auto y_offset = -floor(sqrt((rules.range + 0.5)^2 - x_offset^2)); y_offset <= floor(sqrt((rules.range + 0.5)^2 - x_offset^2)); ++y_offset) {  
-                 to_find = std::make_pair(i,j);
-                 finder = (*influence_map).find(to_find)
-                 if finder == (*influence_map).end(){           // nie ma takiego klucza https://stackoverflow.com/questions/1939953/how-to-find-if-a-given-key-exists-in-a-c-stdmap
-                     (*influence_map).insert(std::pair<to_find, 1);
-                 }
-                 else{                                       // jest taki klucz
-                     (*influence_map)[to_find] += 1;
-                 }
-             }        
-        
-    }                 
+                 updateRecord(influence_map, current_x + x_offset, current_y + y_offset)            
+                  
 }
     
 
 std::map<std::pair<int, int>, int>* Game::generateInfluenceMap(){
         
     current_state = state; //niepotrzebne, ale na razie dla czytelnosci kodu sobie siedzi
-    std::map<std::pair<int, int>, int> influenceMap;
+    std::map<std::pair<int, int>, int> influence_map;
         
     for(auto std::iterator<Cell*> it = state.active_cells.begin(); it != state.active_cells.end();){
-        Game::includeCellInfluence(influenceMap, it);            
+        Game::includeCellInfluence(influence_map, it);            
             
     }
-               
+    return influence_map;
 }
 
     
