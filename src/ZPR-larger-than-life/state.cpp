45f4a7f49d3b187b56139fab1e96092b3d1b604e
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-06-01 22:27:44 +0200

    Removing pointers from stuff

--- src/ZPR-larger-than-life/state.cpp
+++ src/ZPR-larger-than-life/state.cpp
@@ -11,27 +11,27 @@
 
     State::State()=default;
     State::~State()=default;
-    State::State(std::set<Cell*> actives, std::set<Cell*> inactives, int it){
+    State::State(std::set<Cell> actives, std::set<Cell> inactives, int it){
         active_cells = actives;
         inactive_cells = inactives;
         it_number = it;
     }
 
 //    void State::changeIteration(int i){}
-    void State::addActiveCell(Cell *cell){active_cells.insert(cell);}
+    void State::addActiveCell(Cell cell){active_cells.insert(cell);}
 
-    void State::removeActiveCell(Cell *cell){
-        for(auto std::iterator<Cell*> it = active_cells.begin(); it != active_cells.end(); )
+    void State::removeActiveCell(Cell cell){
+        for(auto std::iterator<Cell> it = active_cells.begin(); it != active_cells.end(); )
             if(*it == cell)
                 it = active_cells.erase(it);
                 break;
             else
                 ++it;
     }
-    void State::addInactiveCell(Cell *cell){inactive_cells.insert(cell);}
+    void State::addInactiveCell(Cell cell){inactive_cells.insert(cell);}
 
-    void State::removeInactiveCell(Cell *cell){
-        for(auto std:iterator<Cell*> it = inactive_cells.begin(); it != inactive_cells.end(); )
+    void State::removeInactiveCell(Cell cell){
+        for(auto std:iterator<Cell> it = inactive_cells.begin(); it != inactive_cells.end(); )
             if(*it == cell)
                 it = inactive_cells.erase(it);
                 break;
@@ -39,8 +39,8 @@
                 ++it;        
     }
 
-    std::set<Cell*> State::get_activeCells(){return active_cells;}
-    std::set<Cell*> State::get_inactiveCells(){return inactive_cells;}
+    std::set<Cell> State::get_activeCells(){return active_cells;}
+    std::set<Cell> State::get_inactiveCells(){return inactive_cells;}
     int State::getItNumber(){return it_number;}
     void State::setActiveCells(std::set<Cell> actives){active_cells = actives}
     void State::setInactiveCells(std::set<Cell> inactives){}
