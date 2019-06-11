bab161aa66384bd6fa00064705aba67630dbb62b
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-06-11 18:55:23 +0200

    update TESTS 2

--- src/ZPR-larger-than-life/TESTS.cpp
+++ src/ZPR-larger-than-life/TESTS.cpp
@@ -9,15 +9,13 @@
 
 BOOST_AUTO_TEST_SUITE (CellSuite)
     
-BOOST_AUTO_TEST_CASE (testCellConstruct)
-{
+BOOST_AUTO_TEST_CASE (testCellConstruct){
     BOOST_CHECK (Cell(1,2,3).coords == std::make_pair(1,2));
     BOOST_CHECK (Cell(1,2,3).state == 3);
 
 }
 
-BOOST_AUTO_TEST_CASE (overloadTest)
-{
+BOOST_AUTO_TEST_CASE (overloadTest){
     BOOST_CHECK (Cell(1,2,3) == Cell(1,2,4));
     BOOST_CHECK (!(Cell(2,4,6) == Cell(2,8,6)));
     BOOST_CHECK (!(Cell(2,4,6) == Cell(4,2,6)));
@@ -25,8 +23,7 @@ BOOST_AUTO_TEST_CASE (overloadTest)
     BOOST_CHECK (!(Cell(2,2,2) < Cell(2,2,3)));
 }
 
-BOOST_AUTO_TEST_CASE (testSetGetConstruct)
-{
+BOOST_AUTO_TEST_CASE (testSetGetConstruct){
     Cell cell_1 = Cell(1,2,3);
     cell_1.setCoords(4,5);
     BOOST_CHECK (cell_1.getCoords() == std::make_pair(4,5));
@@ -38,8 +35,7 @@ BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE (StateSuite)
 
-BOOST_AUTO_TEST_CASE (testStateConstruct)
-{
+BOOST_AUTO_TEST_CASE (testStateConstruct){
     std::set<Cell> test_set1;
     test_set1.insert(Cell(1,2,3));
     std::set<Cell> test_set2;
@@ -52,8 +48,7 @@ BOOST_AUTO_TEST_CASE (testStateConstruct)
 
 
 
-BOOST_AUTO_TEST_CASE (testAddingStateCells)
-{
+BOOST_AUTO_TEST_CASE (testAddingStateCells){
     std::set<Cell> test_set1;
     std::set<Cell> test_set2;
     State state_t = State(test_set1, test_set2, 0);
@@ -62,8 +57,7 @@ BOOST_AUTO_TEST_CASE (testAddingStateCells)
 
 }
 
-BOOST_AUTO_TEST_CASE (testRemovingStateCells)
-{
+BOOST_AUTO_TEST_CASE (testRemovingStateCells){
     std::set<Cell> test_set1;
     std::set<Cell> test_set2;
     test_set2.insert(Cell(1,2,3));
@@ -77,8 +71,7 @@ BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE (ChangeSuite)
 
-BOOST_AUTO_TEST_CASE (testChangeConstructor)
-{
+BOOST_AUTO_TEST_CASE (testChangeConstructor){
     std::list<Cell> test_list1;
     std::list<Cell> test_list2;
     test_list1.push_back(Cell(1,2,3));
@@ -87,8 +80,7 @@ BOOST_AUTO_TEST_CASE (testChangeConstructor)
     BOOST_CHECK (Change(test_list1, test_list2, 0).getToBirth == test_list2);
 }
 
-BOOST_AUTO_TEST_CASE (testAddingChangeCells)
-{
+BOOST_AUTO_TEST_CASE (testAddingChangeCells){
     std::list<Cell> test_list1;
     std::list<Cell> test_list2;
     Change change_t = Change(test_set1, test_set2, 0);
@@ -96,8 +88,7 @@ BOOST_AUTO_TEST_CASE (testAddingChangeCells)
     BOOST_CHECK (state_t.getToBirth().size() == 1);
 }
 
-BOOST_AUTO_TEST_CASE (testRemovingChangeCells)
-{
+BOOST_AUTO_TEST_CASE (testRemovingChangeCells){
     std::list<Cell> test_set1;
     std::list<Cell> test_set2;
     test_set2.addInactiveCell(Cell(1,2,3));
@@ -106,8 +97,7 @@ BOOST_AUTO_TEST_CASE (testRemovingChangeCells)
     BOOST_CHECK (change_t.getInactiveCells().size() == 0);
 }
 
-BOOST_AUTO_TEST_CASE (testRemovingChangeCells2)
-{
+BOOST_AUTO_TEST_CASE (testRemovingChangeCells2){
     std::list<Cell> test_set1;
     std::list<Cell> test_set2;
     test_set2.addInactiveCell(Cell(1,2,3));
@@ -115,3 +105,18 @@ BOOST_AUTO_TEST_CASE (testRemovingChangeCells2)
     State state_t = State(test_set1, test_set2, 0);
     BOOST_CHECK (change_t.getInactiveCells().size() == 1);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
+                                 
+
+BOOST_AUTO_TEST_SUITE(GameSuite)
+                                 
+BOOST_AUTO_TEST_CASE (EasyFunctions){
+    Rules rules = Rules(NeighbourhoodType::Moore, 1, 1, 1, 1, 1, 1, 1);
+    std::deque<Change> test_changes;
+    test_changes.push_front(Change());
+    State test_state = State();
+    BOOST_CHECK ( Gane(rules, test_changes, test_state).getRules == rules);    
+}
+                                 
+                                 
