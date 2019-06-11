a06001e4c03687c8043bd6ebb6bfb14410283c90
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-06-11 12:35:05 +0200

    doxyed

--- src/ZPR-larger-than-life/change.cpp
+++ src/ZPR-larger-than-life/change.cpp
@@ -5,14 +5,20 @@
 #include "cell.hpp"
 #include <list>
 
+//! Klasa Change prezentujaca odpowiednie zmiany komorek pomiedzy dwoma iteracjami
+//! Pola klasy:
+//! std::list<Cell> toShift - lista komorek ktore w tej iteracji zmniejszana swoj stan
+//! std::list<Cell> toBirth - lista komorek ktore w tej iteracji sie "rodza" (ustawiaja swoj stan na maksymalny)
 
 
-
+    //! Konstruktor dwuelementowy klasy Change przyjmujacy analogiczne parametry co pola klasy
     Change::Change(std::list<Cell> toshifts, std::list<Cell> tobirths){
         toShift = toshifts;
         toBirth = tobirths;
     }
+    //! Konstruktor domyslny
     Change::Change() = default;
+    //! Destruktor domyslny
     Change::~Change() = default;
 
     std::list<Cell> Change::getToShift(){return toShift;}
@@ -21,7 +27,13 @@
     void Change::setToShift(std::list<Cell> toshifted){toShift = toshifted;}
     void Change::setToBirth(std::list<Cell> tobirthed){toBirth = tobirthed;}
 
+    //! Pomocnicza funkcja umozliwiajaca dostawianie obiektow typu Cell do pola toShift
+    //! Przyjmowane argumenty:
+    //! Cell toshifted - komorka ktora ma byc dodana do pola ToShift
     void Change::addToShift(Cell toshifted){toShift.push_back(toshifted);}
+    //! Pomocnicza funkcja umozliwiajaca dostawianie obiektow typu Cell do pola toBirth
+    //! Przyjmowane argumenty:
+    //! Cell tosbirthed - komorka ktora ma byc dodana do pola ToBirth
     void Change::addToBirth(Cell tobirthed){toShift.push_back(tobirthed);}
 
 /*
