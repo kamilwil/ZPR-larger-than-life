efb2ed4a215307582f354e295b756e669f2fabd2
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-06-11 12:57:31 +0200

    Doxed

--- src/ZPR-larger-than-life/rules.cpp
+++ src/ZPR-larger-than-life/rules.cpp
@@ -1,6 +1,16 @@
 #include "rules.hpp"
+//* Struktura rules przechowujaca odpowidnie wartosci zasad obecnych w danej grze
+//* Pola klasy:
+//* NeighbourhoodType neighbourhood - typ sasiedztwa
+//* int range - zasieg sasiedztwa
+//* int states - ilosc stanow
+//* int smin - minimalna ilosc sasiadow do przezycia komorki
+//* int smax - maksymalna ilosc sasiadow do przezycia komorki
+//* int bmin - minimalna ilosc sasiadow do powstania komorki
+//* int bmax - maksymalna ilosc sasiadow do powstania komorki
 
-// Implementacja konstruktora struktury rules
+
+//* Implementacja konstruktora struktury rules o argumentach analogicznych do pol klasy
 Rules::Rules(NeighbourhoodType n, int r, int c, int sminimum, int smaximum, int bminimum, int bmaximum, int flag){
 	neighbourhood = n;
 	range = r;
@@ -12,5 +22,7 @@ Rules::Rules(NeighbourhoodType n, int r, int c, int sminimum, int smaximum, int 
 	m = flag;
 }
 
+//* Domyslny konstruktor klasy Rules
 Rules::Rules() = default;
+//* Domyslny destruktor klasy Rules
 Rules::~Rules() = default;
