a5555648a9f0a94ddf86ce9a86e2a86d8f7337b9
Piotr Roszkowski <36736903+proszko@users.noreply.github.com>
2019-06-11 12:25:09 +0200

    added doxycomments

--- src/ZPR-larger-than-life/cell.cpp
+++ src/ZPR-larger-than-life/cell.cpp
@@ -1,15 +1,21 @@
-//Cell.cpp
+
 #include "cell.hpp"
 #include <utility>
 
-    Cell::Cell() = default;
-    Cell::~Cell() = default;
-    Cell::Cell(int x, int y, int st){
+//! Klasa Cell obslugujaca pojedyncza pola planszy i przechowuje jej stan.
+//! Pola klasy:
+//! std::pair coords - przechowuje koordynaty danej komorki
+//! int state - przechowuje stan (wartosc liczbowa) danej komorki
+
+    
+    Cell::Cell() = default;             //!< Domyslny konstruktor klasy
+    Cell::~Cell() = default;            //!< Domyslny destruktor klasy
+    Cell::Cell(int x, int y, int st){   //!< Trojargumentowy konstruktor klasy przyjmujacy argumenty analogiczne do pol klasy
         coords = std::make_pair(x,y);
         state = st;
     }
         
-    void Cell::setCoords(int x, int y){coords.first = x; coords.second = y;}
+    void Cell::setCoords(int x, int y){coords.first = x; coords.second = y;}   
     std::pair<int,int> Cell::getCoords(){return coords;}
     int Cell::getXcoord(void){return coords.first;}
     int Cell::getYcoord(void){return coords.second;}
